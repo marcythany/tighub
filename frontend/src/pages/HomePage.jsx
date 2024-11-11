046ab30eab4e7b976d1cbb1a3415aad518c9e80f
Marcy Thany <marcythany@gmail.com>
2024-11-10 21:33:38 -0300

    feat: funções de pesquisa e sort adicionadas.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,37 +15,66 @@ const HomePage = () => {
   const [sortType, setSortType] = useState('forks');
   const user = true;
 
-  const getUserProfileAndRepos = useCallback(async () => {
-    setLoading(true);
-    try {
-      const userRes = await fetch('https://api.github.com/users/marcythany');
-      const userProfile = await userRes.json();
-      setUserProfile(userProfile);
+  const getUserProfileAndRepos = useCallback(
+    async (username = 'marcythany') => {
+      setLoading(true);
+      try {
+        const userRes = await fetch(`https://api.github.com/users/${username}`);
+        const userProfile = await userRes.json();
+        setUserProfile(userProfile);
 
-      const reposRes = await fetch(userProfile.repos_url);
-      const repos = await reposRes.json();
-      setRepos(repos);
-      console.log('userProfile:', userProfile);
-      console.log('repos:', repos);
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+        const reposRes = await fetch(userProfile.repos_url);
+        const repos = await reposRes.json();
+        setRepos(repos);
+        console.log('userProfile:', userProfile);
+        console.log('repos:', repos);
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [],
+  );
 
   useEffect(() => {
     getUserProfileAndRepos();
   }, [getUserProfileAndRepos]);
 
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
+
+  const onSort = (sortType) => {
+    if (sortType === 'recent') {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (sortType === 'stars') {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    } else if (sortType === 'forks') {
+      repos.sort((a, b) => b.forks_count - a.forks_count);
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
-      <Search />
-      <SortRepos />
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex flex-col items-start justify-center gap-4 lg:flex-row">
-        <ProfileInfo userProfile={userProfile} />
-        <Repos />
-        <Spinner />
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
     </div>
   );
