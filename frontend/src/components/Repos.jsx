8f9567a8fcc43407d5cdb036130dbfdb05fe8822
Marcy Thany <marcythany@gmail.com>
2024-11-10 21:30:36 -0300

    feat: criação do mapa para gerar os repos

--- frontend/src/components/Repos.jsx
+++ frontend/src/components/Repos.jsx
@@ -1,12 +1,17 @@
 import Repo from './Repo';
 
-const Repos = () => {
+const Repos = ({ repos }) => {
   return (
     <div className={`bg-glass w-full rounded-lg px-8 py-6 lg:w-2/3`}>
       <ol className="relative border-s border-gray-200">
-        <Repo />
-        <Repo />
-        <Repo />
+        {repos.map((repo) => (
+          <Repo key={repo.id} repo={repo} />
+        ))}
+        {repos.legth === 0 && (
+          <p className="flex h-32 items-center justify-center">
+            Nenhum repositório encontrado
+          </p>
+        )}
       </ol>
     </div>
   );
