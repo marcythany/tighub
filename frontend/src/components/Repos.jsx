f51324bf12aba14c8706adb567c5dc38a12df594
Marcy Thany <marcythany@gmail.com>
2024-11-11 17:19:18 -0300

    feat: criação de variavel dinamica para estilização

--- frontend/src/components/Repos.jsx
+++ frontend/src/components/Repos.jsx
@@ -1,8 +1,9 @@
 import Repo from './Repo';
 
-const Repos = ({ repos }) => {
+const Repos = ({ repos, alwaysFullWidth = false }) => {
+  const className = alwaysFullWidth ? 'w-full' : 'lg:w-2/3 w-full';
   return (
-    <div className={`bg-glass w-full rounded-lg px-8 py-6 lg:w-2/3`}>
+    <div className={`${className} bg-glass rounded-lg px-8 py-6`}>
       <ol className="relative border-s border-gray-200">
         {repos.map((repo) => (
           <Repo key={repo.id} repo={repo} />
