6effdadcf613da602f0f177f8e38068103bb2fe9
Marcy Thany <marcythany@gmail.com>
2024-11-14 13:12:00 -0300

    feat: adição do hook de autenticação

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,17 +1,34 @@
 import { MdLogout } from 'react-icons/md';
-// TODO Implement Logout functionality
+import { useAuthContext } from '../context/AuthContext';
+import { toast } from 'react-hot-toast';
 
 const Logout = () => {
+  // Desestruture authUser e setAuthUser diretamente no componente
+  const { authUser, setAuthUser } = useAuthContext();
+
+  const handleLogout = async () => {
+    try {
+      const res = await fetch('/api/auth/logout', { credentials: 'include' });
+      const data = await res.json();
+      console.log(data);
+      setAuthUser(null);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <>
       <img
-        src={
-          'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745'
-        }
+        src={authUser?.avatarUrl}
         className="h-10 w-10 rounded-full border border-gray-800"
+        alt="User Avatar"
       />
 
-      <div className="bg-glass mt-auto flex cursor-pointer items-center rounded-lg border border-gray-800 p-2">
+      <div
+        className="bg-glass mt-auto flex cursor-pointer items-center rounded-lg border border-gray-800 p-2"
+        onClick={handleLogout}
+      >
         <MdLogout size={22} />
       </div>
     </>
