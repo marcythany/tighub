12fec32c3619bba10f70bc66a41ca285b1e2701f
Marcy Thany <marcythany@gmail.com>
2024-11-14 11:06:39 -0300

    feat: adição conexão com DB e Rota de Autenticação

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import exploreRoutes from './routes/exploreRoutes.js';
+import ConnectDB from './db/connectDB.js';
 
 dotenv.config();
 
@@ -16,9 +18,11 @@ app.get('', async (req, res) => {
 	res.send('Tô de pé');
 });
 
+app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/explore/', exploreRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server está rodando em http://localhost:${PORT}`);
+	ConnectDB();
 });
