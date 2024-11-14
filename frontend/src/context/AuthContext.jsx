1f174109b0f399e7a42df55cca9a5dc270133857
Marcy Thany <marcythany@gmail.com>
2024-11-14 13:58:43 -0300

    fix: loading adicionado
    Para evitar volta a pagina inicial quando atualiza-se a pagina.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -7,23 +7,26 @@ export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(null);
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkUserLoggedIn = async () => {
+      setLoading(true);
       try {
         const res = await fetch('/api/auth/check', { credentials: 'include' });
         const data = await res.json();
         setAuthUser(data.user);
       } catch (error) {
         toast.error(error);
+      } finally {
+        setLoading(false);
       }
     };
     checkUserLoggedIn();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+    <AuthContext.Provider value={{ authUser, setAuthUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
