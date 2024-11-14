2bc174bc09f0e0dcafd2fdf603e35be60c7e2d4a
Marcy Thany <marcythany@gmail.com>
2024-11-14 13:56:20 -0300

    feat: logica para curtir perfils

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import githubAPI from '../services/github.js';
+import User from '../models/userModels.js';
 
 export const getUserProfileAndRepos = async (req, res) => {
 	const { username } = req.params;
@@ -13,5 +14,29 @@ export const getUserProfileAndRepos = async (req, res) => {
 	} catch (error) {
 		console.error('Erro ao buscar dados do GitHub:', error);
 		res.status(500).json({ error: error.message });
+	}
+};
+
+export const likeProfile = async (req, res) => {
+	try {
+		const { username } = req.params;
+		const user = await User.findById(req.user._id.toString());
+		console.log('Usuário autenticado', user);
+		const userToLike = await User.findOneAndDelete({ username });
+
+		if (!userToLike) {
+			return res.status(404).json({ error: 'Usuário não é um membro.' });
+		}
+
+		if (user.likedProfiles.includes(userToLike.username)) {
+			return res.status(400).json({ error: 'Você já deu like nesse perfil.' });
+		}
+		userToLike.likedBy.push({
+			username: user.username,
+			avatarUrl: user.avatarUrl,
+			likedDate: Date.now(),
+		});
+	} catch (error) {
+		restart.status(500).json({ error: error.message });
 	}
 };
--- frontend/src/pages/LikesPage.jsx
+++ frontend/src/pages/LikesPage.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import { FaHeart } from 'react-icons/fa';
+import toast from 'react-hot-toast';
+import { formatDate } from '../utils/functions';
 
 const LikesPage = () => {
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    const getLikes = async () => {
+      try {
+        const res = await fetch('/api/users/likes', { credentials: 'include' });
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+
+        setLikes(data.likedBy);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    getLikes();
+  }, []);
+  console.log('likes:', likes);
+
   return (
     <div className="relative overflow-x-auto rounded-lg px-4 shadow-md">
       <table className="bg-glass w-full overflow-hidden text-left text-sm rtl:text-right">
@@ -21,39 +42,38 @@ const LikesPage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-glass border-b">
-            <td className="w-4 p-4">
-              <div className="flex items-center">
-                <span>1</span>
-              </div>
-            </td>
-            <th
-              scope="row"
-              className="flex items-center whitespace-nowrap px-6 py-4"
-            >
-              <img
-                className="h-10 w-10 rounded-full"
-                src={
-                  'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745'
-                }
-                alt="Jese image"
-              />
-              <div className="ps-3">
-                <div className="text-base font-semibold">dasdas</div>
-              </div>
-            </th>
-            <td className="px-6 py-4">das</td>
-            <td className="px-6 py-4">
-              <div className="flex items-center">
-                <FaHeart size={22} className="mx-2 text-red-500" />
-                Liked your profile
-              </div>
-            </td>
-          </tr>
+          {likes.map((user, idx) => (
+            <tr className="bg-glass border-b" key={user.username}>
+              <td className="w-4 p-4">
+                <div className="flex items-center">
+                  <span>{idx + 1}</span>
+                </div>
+              </td>
+              <th
+                scope="row"
+                className="flex items-center whitespace-nowrap px-6 py-4"
+              >
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={user.avatarUrl}
+                  alt="User Avatar"
+                />
+                <div className="ps-3">
+                  <div className="text-base font-semibold">{user.username}</div>
+                </div>
+              </th>
+              <td className="px-6 py-4">{formatDate(user.likedDate)}</td>
+              <td className="px-6 py-4">
+                <div className="flex items-center">
+                  <FaHeart size={22} className="mx-2 text-red-500" />
+                  Liked your profile
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
 };
-
 export default LikesPage;
