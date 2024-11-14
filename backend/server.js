02e8b9b26657e15f635dd318cae0260b665e0e2c
Marcy Thany <marcythany@gmail.com>
2024-11-14 13:08:10 -0300

    feat: configuração do passport ao backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import passport from 'passport';
+import session from 'express-session';
+
+import './passport/githubAuth.js';
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,6 +16,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+	session({ secret: 'keyboard cat', resave: false, saveUninitialized: false })
+);
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 
 app.get('', async (req, res) => {
