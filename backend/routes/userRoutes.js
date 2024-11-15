86467105abfa5376ad4ddcdb9a7038862fc72941
Marcy Thany <marcythany@gmail.com>
2024-11-15 00:05:27 -0300

    feat: criação das rotas de perfil e likes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,17 @@ import express from 'express';
 import {
 	getUserProfileAndRepos,
 	likeProfile,
+	getLikes,
+	getUserProfile,
 } from '../controllers/userController.js';
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated.js';
 
 const router = express.Router();
 
+router.get('/profile', ensureAuthenticated, getUserProfile);
 router.get('/profile/:username', getUserProfileAndRepos);
 
+router.get('/likes', ensureAuthenticated, getLikes);
 router.post('/like/:username', ensureAuthenticated, likeProfile);
 
 export default router;
