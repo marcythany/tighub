eb7c694e191c9b50deb5bea5e1f6d8917a08b643
Marcy Thany <marcythany@gmail.com>
2024-11-15 00:02:49 -0300

    feat: logica para mostrar se usuário foi curtido ou não.

--- frontend/src/components/LikeProfile.jsx
+++ frontend/src/components/LikeProfile.jsx
@@ -1,11 +1,38 @@
 import { FaHeart } from 'react-icons/fa';
 import toast from 'react-hot-toast';
 import { useAuthContext } from '../context/AuthContext';
+import { useState, useEffect } from 'react';
 
 const LikeProfile = ({ userProfile }) => {
   const { authUser } = useAuthContext();
+  const [liked, setLiked] = useState(false);
 
   const isOwnProfile = authUser?.username === userProfile.login;
+
+  useEffect(() => {
+    const fetchLikedProfiles = async () => {
+      try {
+        const res = await fetch('/api/users/likes', {
+          method: 'GET',
+          credentials: 'include',
+        });
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+
+        // Verifica se o perfil está na lista de perfis curtidos
+        const isLiked = data.likedProfiles.some(
+          (profile) => profile.username === userProfile.login,
+        );
+        setLiked(isLiked);
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+
+    if (authUser) {
+      fetchLikedProfiles();
+    }
+  }, [authUser, userProfile]);
 
   const handleLikeProfile = async () => {
     try {
@@ -16,6 +43,8 @@ const LikeProfile = ({ userProfile }) => {
       const data = await res.json();
 
       if (data.error) throw new Error(data.error);
+
+      setLiked((prevLiked) => !prevLiked); // Atualiza o estado de liked
       toast.success(data.message);
     } catch (error) {
       toast.error(error.message);
@@ -26,11 +55,14 @@ const LikeProfile = ({ userProfile }) => {
 
   return (
     <button
-      className="bg-glass flex w-full items-center gap-2 rounded-md border border-blue-400 p-2 text-xs font-medium"
+      className={`bg-glass flex w-full items-center gap-2 rounded-md border border-blue-400 p-2 text-xs font-medium ${
+        liked ? 'text-red-500' : 'text-gray-400'
+      }`}
       onClick={handleLikeProfile}
     >
-      <FaHeart size={16} /> Curtir o Perfil
+      <FaHeart size={16} /> {liked ? 'Curtido' : 'Curtir o Perfil'}
     </button>
   );
 };
+
 export default LikeProfile;
