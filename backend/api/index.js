a7eaa5eb85d06fdb0acf4ef2c91bd1fe97fe9999
Marcy Thany <marcythany@gmail.com>
2024-11-15 04:14:32 -0300

    Delete index.js

--- backend/api/index.js
+++
@@ -1,4 +0,0 @@
-import { createServer } from '@vercel/node';
-import app from '../server'; // Importando o app do backend
-
-export default createServer(app); // Expondo o app como uma função serverless
