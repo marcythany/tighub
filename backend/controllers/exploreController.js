28eb79f49372da5b5d45ae4afb0907ad85aa8d37
Marcy Thany <marcythany@gmail.com>
2024-11-16 16:07:40 -0300

    fix: alteração na logica do explorar.

--- backend/controllers/exploreController.js
+++ backend/controllers/exploreController.js
@@ -1,21 +1,20 @@
-import githubAPI from '../services/github.js';
-
 export const explorePopularRepos = async (req, res) => {
 	const { language } = req.params;
 
 	try {
-		if (!language) {
-			return res.status(400).json({ error: 'Linguagem não válida' });
-		}
+		// 5000 requests per hour for authenticated requests
+		const response = await fetch(
+			`https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=10`,
+			{
+				headers: {
+					authorization: `token ${process.env.GITHUB_API_KEY}`,
+				},
+			}
+		);
+		const data = await response.json();
 
-		// Chamando o método getLanguages para buscar os languages
-		const data = await githubAPI.getRepositories(language);
-
-		res.status(200).json({ data });
+		res.status(200).json({ repos: data.items });
 	} catch (error) {
-		console.error('Error fetching languages:', error);
-		res.status(500).json({
-			error: error.message || 'Erro ao buscar linguagens no repositório',
-		});
+		res.status(500).json({ error: error.message });
 	}
 };
