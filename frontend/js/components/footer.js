22d5e424d45cbf33ca9ab51455e6937146fbf1f6
Marcy Thany <marcythany@gmail.com>
2024-11-27 11:09:16 -0300

    fix: ajustes no footer

--- frontend/js/components/footer.js
+++ frontend/js/components/footer.js
@@ -8,17 +8,7 @@ export function createFooter() {
     function updateContent() {
         footer.innerHTML = `
             <div class="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-                <div class="flex flex-wrap justify-center md:justify-start gap-4">
-                    <a href="/about" class="hover:text-teal-600 dark:hover:text-teal-400 transition-colors">
-                        ${languageManager.translate('about')}
-                    </a>
-                    <a href="/privacy" class="hover:text-teal-600 dark:hover:text-teal-400 transition-colors">
-                        ${languageManager.translate('privacyPolicy')}
-                    </a>
-                    <a href="/terms" class="hover:text-teal-600 dark:hover:text-teal-400 transition-colors">
-                        ${languageManager.translate('termsOfService')}
-                    </a>
-                </div>
+
                 <div class="text-sm text-teal-700 dark:text-teal-300">
                     ${languageManager.translate('copyright')}
                 </div>
